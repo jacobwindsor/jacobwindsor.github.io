4c7954a53af8418529779f70e3e533ccae16748e
Jacob Windsor <me@jcbwndsr.com>
2020-08-04 02:56:28 +0200

    Updates

---
+++ precache-manifest.b7faec7e72e665aee56f17c7e6f8e4e9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd6052e02810576eaa8764dc0658f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9964b259bc38e88983e0",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "76c9b21b1a900a11201f",
+    "url": "/static/css/main.ae7cdc1b.chunk.css"
+  },
+  {
+    "revision": "9964b259bc38e88983e0",
+    "url": "/static/js/2.cc9aa12a.chunk.js"
+  },
+  {
+    "revision": "fe92b81bd0c9a8fb7f58b2871267645f",
+    "url": "/static/js/2.cc9aa12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c9b21b1a900a11201f",
+    "url": "/static/js/main.9ec0fdf6.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "cf39b1f6a9ce4ab12162f12cf7375370",
+    "url": "/static/media/MetabMaster.cf39b1f6.png"
+  },
+  {
+    "revision": "7a6150cb4435cb2ec5de574a835d51d5",
+    "url": "/static/media/PathwayPresenter.7a6150cb.png"
+  },
+  {
+    "revision": "51d669cf8bc32b6fc23b98fe4bbf2a53",
+    "url": "/static/media/SQANTI-Explorer.51d669cf.png"
+  },
+  {
+    "revision": "b00df16a43dfc0fb9d31e83fe57a3bd5",
+    "url": "/static/media/abcam.b00df16a.png"
+  },
+  {
+    "revision": "f730112c65acb68c51d39b254a87fede",
+    "url": "/static/media/crg.f730112c.png"
+  },
+  {
+    "revision": "0e89fe001da31ab081f001c561fdb1ee",
+    "url": "/static/media/gsoc.0e89fe00.png"
+  },
+  {
+    "revision": "b446f1c57c7e6abc217fcefd6c8ae702",
+    "url": "/static/media/jReact.b446f1c5.png"
+  },
+  {
+    "revision": "1c559d7904a433ed212b0af33804b8d7",
+    "url": "/static/media/profile.1c559d79.jpg"
+  },
+  {
+    "revision": "8f801b841a5d318f89b5f26fcdafb979",
+    "url": "/static/media/pubchem.8f801b84.png"
+  },
+  {
+    "revision": "df70b00589269cc7e903d0e3ba9db627",
+    "url": "/static/media/react.df70b005.png"
+  },
+  {
+    "revision": "34007e78a75e59a8ea352047661b4a8c",
+    "url": "/static/media/tmerge.34007e78.png"
+  },
+  {
+    "revision": "60d29763311581a85dafbf1ac4bdcdbe",
+    "url": "/static/media/webpackify.60d29763.png"
+  },
+  {
+    "revision": "2725e3a2fa6ba50d04c489398d60473a",
+    "url": "/static/media/wikipathways.2725e3a2.png"
+  }
+]);
