58ef047e8d93c321537989cf7b6d0763190259a0
Jacob Windsor <jwindsor@axiomatic-ai.com>
2025-10-31 04:30:26 +0100

    chore: modernize

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,31 @@
 /// <reference path="../.astro/types.d.ts" />
+
+declare module '*.jpg' {
+  const value: { src: string; width: number; height: number; format: string };
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: { src: string; width: number; height: number; format: string };
+  export default value;
+}
+
+declare module '*.png' {
+  const value: { src: string; width: number; height: number; format: string };
+  export default value;
+}
+
+declare module '*.webp' {
+  const value: { src: string; width: number; height: number; format: string };
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: { src: string; width: number; height: number; format: string };
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: { src: string; width: number; height: number; format: string };
+  export default value;
+}
